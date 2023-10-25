75515f1380362d3f57f6bb997afff44ea77444e7
GeorgeMurphy-1 <0GeorgeMurphy@gmail.com>
2023-10-25 15:19:57 -0400

    config(database, redis, CORS)

---
+++ medusa-config.js
@@ -0,0 +1,97 @@
+const dotenv = require("dotenv");
+
+//Database connection details for production on digital ocean: Digital ocean
+// provides connection details in seperate parts referrence :https://docs.medusajs.com/deployments/server/deploying-on-digital-ocean#changes-to-medusa-configjs
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DB_DATABASE;
+
+
+const DATABASE_URL =
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: `@medusajs/file-local`,
+    options: {
+      upload_dir: "uploads",
+    },
+  },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      develop: {
+        open: process.env.OPEN_BROWSER !== "false",
+      },
+    },
+  },
+];
+
+const modules = {
+  modules: {
+    eventBus: {
+      resolve: "@medusajs/event-bus-redis",
+      options: { 
+        redisUrl: process.env.EVENTS_REDIS_URL,
+      },
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        redisUrl: process.env.CACHE_REDIS_URL,
+        ttl: 30,
+      },
+    },
+  },
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  admin_cors: process.env.ADMIN_CORS,
+  store_cors: process.env.STORE_CORS,
+  database_extra: { ssl: { rejectUnauthorized: false } },
+  database_logging: ["error", "query"],
+  database_url: process.env.DATABASE_URL,
+  redis_url: process.env.REDIS_URL,
+  redis_prefix: "e-bike-repair",
+  redis_options: {
+    connectionName: "medusa",
+  },
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+  modules,
+};
